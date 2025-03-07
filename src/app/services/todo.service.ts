8e281756fcfd33e1427de035c64f67acbeda093b
MaLoY6223 <mloy6223@gmail.com>
2025-03-07 18:40:57 +0300

    Commit with a mistake

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { Interface } from 'readline/promises';
 
-interface Todo {
+export interface Todo {
   userId: number;
   id: number;
   title: string;
