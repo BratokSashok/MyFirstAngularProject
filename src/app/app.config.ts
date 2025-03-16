af27599e306fac1ab85134d4424b4a9d0e1cbaf4
MaLoY6223 <mloy6223@gmail.com>
2025-03-16 19:36:09 +0300

    sdelal vse

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import bootstrap from '../main.server';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,3 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay())
     ]
 };
+
+bootstrapApplication( AddComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routers)
+  ]
+}).catch((err) => console.error(err));
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoDetailComponent } from './pages/todo-detail/todo-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: "", component: HomeComponent },
+    { path: "todos", component: TodoComponent },
+    { path: "todos/:id", component: TodoDetailComponent }
+    /*,
+    { path: "about", component: AboutComponent },
+    { path: "**", component: NotFoundComponent }*/
+];
